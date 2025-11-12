a9fd89dec53d31ccc2115e5a38905d93f45edea8
Rashed Albalawi <252933012+alb-rashed@users.noreply.github.com>
2025-11-12 10:16:51 +0300

    g

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,5 @@
 from . import numpy_util
 from . import pytorch_util
 from . import quat_util
-
-__all__ = ["numpy_util", "pytorch_util", "quat_util"]
+from . import array_compat
+__all__ = ["numpy_util", "pytorch_util", "quat_util", "array_compat"]
