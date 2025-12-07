88de29dc6e2a6f706893bd271aa0316b11326a5d
Hasan Al Thobaiti <193288475+hasan-fahad1@users.noreply.github.com>
2025-12-07 04:28:41 +0300

    g

--- rlc_ws/src/rlc_sim/rlc_sim/sim_viewer_node.py
+++ rlc_ws/src/rlc_sim/rlc_sim/sim_viewer_node.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import mujoco as mj
 from mujoco import viewer # type: ignore
+
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Trigger, SetBool
@@ -12,8 +13,8 @@ from common_utils import ros_util as ru
 
 from rlc_common.endpoints import (
     TOPICS, SERVICES, ACTIONS, 
-    JointStateMsg, JointEffortCmdMsg,
-    ResetSimSrv, PauseSimSrv,
+    JointStateMsg, JointEffortCmdMsg, PlannedCartTrajMsg, CurrentPlanMsg,
+    ResetSimSrv, PauseSimSrv, FrameStatesMsg
 )
 
 
@@ -25,7 +26,6 @@ class Gen3MujocoVizNode(Node):
     - Subscribes to the headless sim joint state topic
     - Mirrors joint positions into d.qpos and calls mj_forward in the render loop
     """
-
     def __init__(self) -> None:
         super().__init__("gen3_mujoco_viz")
 
@@ -50,6 +50,24 @@ class Gen3MujocoVizNode(Node):
             self.joint_state_callback,
             10,
         )
+        self.cart_traj_sub = self.create_subscription(
+            TOPICS.planned_cart_traj.type,
+            TOPICS.planned_cart_traj.name,
+            self.cart_traj_callback,
+            10,
+        )
+        self.current_plan_sub = self.create_subscription(
+            TOPICS.current_plan.type,
+            TOPICS.current_plan.name,
+            self.current_plan_callback,
+            10,
+        )
+        self.frame_states_sub = self.create_subscription(
+            TOPICS.frame_states.type,
+            TOPICS.frame_states.name,
+            self.frame_state_callback,
+            10,
+        )
 
         self.reset_client = self.create_client(
             SERVICES.reset_sim.type, 
@@ -64,31 +82,37 @@ class Gen3MujocoVizNode(Node):
             f"Gen3 MuJoCo viz node ready. Listening to {TOPICS.joint_state.name}"
         )
 
+
+    def _invalidate_frames(self) -> None:
+        if not self.frames_dirty: self.frames_dirty = True 
+
+
+    def _pause_sim(self, pause: bool) -> None:
+        if self.pause_client.wait_for_service(timeout_sec=1.0):
+            req = PauseSimSrv.Request()
+            req.data = pause
+            future = self.pause_client.call_async(req)
+            self.get_logger().info("pause/unpause service called")
     
+
+    def _reset_sim(self) -> None:
+        if self.reset_client.wait_for_service(timeout_sec=1.0):
+            req = ResetSimSrv.Request()
+            future = self.reset_client.call_async(req)
+            self.get_logger().info("Reset service called")
+
+
     def key_callback(self, keycode):
         c = chr(keycode)
         match c:
             case " ":
-                if self.pause_client.wait_for_service(timeout_sec=1.0):
-                    req = PauseSimSrv.Request()
-                    req.data = False
-                    future = self.pause_client.call_async(req)
-                    self.get_logger().info("pause/unpause service called")
-
+                self._pause_sim(False) if self.paused else self._pause_sim(True)
             case "r" | "R":
-                if self.reset_client.wait_for_service(timeout_sec=1.0):
-                    req = ResetSimSrv.Request()
-                    future = self.reset_client.call_async(req)
-                    self.get_logger().info("Reset service called")
-                
-                if self.pause_client.wait_for_service(timeout_sec=1.0):
-                    req = PauseSimSrv.Request()
-                    req.data = False
-                    future = self.pause_client.call_async(req)
-                    self.get_logger().info("pause/unpause service called")
-                    
+                self._reset_sim()
+                self._pause_sim(True)
             case "1":
                 self.show_frames = not self.show_frames
+                self._invalidate_frames()
 
             case "j" | "J":
                 self.show_jacobian = True
@@ -103,6 +127,35 @@ class Gen3MujocoVizNode(Node):
                 self.show_com = not self.show_com
 
 
+    def cart_traj_callback(self, msg: PlannedCartTrajMsg) -> None:
+        """
+        Update qpos_target from the latest JointStateSim message.
+
+        Assumes msg.name entries match MuJoCo joint names.
+        """
+        pass
+
+
+    def current_plan_callback(self, msg: CurrentPlanMsg) -> None:
+        """
+        Update qpos_target from the latest JointStateSim message.
+
+        Assumes msg.name entries match MuJoCo joint names.
+        """
+        pass
+
+
+    def frame_state_callback(self, msg: FrameStatesMsg) -> None:
+        """
+        Update qpos_target from the latest JointStateSim message.
+
+        Assumes msg.name entries match MuJoCo joint names.
+        """
+        state = ru.from_pose_array_msg(msg)
+        self.poses = state.poses
+        self._invalidate_frames()
+
+
     def joint_state_callback(self, msg: JointStateMsg) -> None:
         """
         Update qpos_target from the latest JointStateSim message.
@@ -115,6 +168,18 @@ class Gen3MujocoVizNode(Node):
         self.qpos_target = qpos_target
         self.qvel_target = qvel_target
         self.target_t = state.stamp
+
+
+def handle_frame_visuals(env: Gen3Env, viewer: viewer.MjViewer) -> None:
+    """Render frame visuals in the MuJoCo viewer."""
+    if node.frames_dirty and node.frames_exists:
+        # destroy frames
+        if node.show_frames:
+            # create frames
+            node.frames_exists = True
+        else:
+            node.frames_exists = False
+        node.frames_dirty = False
 
 
 def main(args=None) -> None:
@@ -135,6 +200,11 @@ def main(args=None) -> None:
                         qvel=node.qvel_target,
                         t=node.target_t,
                     )
+                    
+                    if self.show_frames and node.frames_dirty:
+                        mj.
+                        node.frames_dirty = False
+                    
                     v.sync()
                 time.sleep(0.0001)
 
