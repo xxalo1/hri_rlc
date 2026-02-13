f93e8bd74238c573df9664c377ce4698847d2541
Hasan Al Thobaiti <193288475+hasan-fahad1@users.noreply.github.com>
2026-02-13 14:55:57 +0300

    Add initial package structure and configuration for rlc_bringup

---
+++ rlc_ws/src/rlc_bringup/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "rlc_bringup"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="g201951870",
+    maintainer_email="asme.kfupm.website@gmail.com",
+    description="Minimal bringup for Franka robots in Gazebo and ROS 2.",
+    license="MIT",
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    entry_points={
+        "console_scripts": [],
+    },
+)
